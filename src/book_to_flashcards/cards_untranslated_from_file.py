ae16bd44b932a6121c94f4f823103ef700cceda7
Ashby Winch <msn@blueumbrella.net>
2024-08-07 17:57:58 +0100

    Moved current/prev code into Anki world

--- src/book_to_flashcards/cards_untranslated_from_file.py
+++ src/book_to_flashcards/cards_untranslated_from_file.py
@@ -4,6 +4,7 @@ from split_sentences import make_nlp, split_sentence
 
 from collections.abc import Generator
 from typing import Any
+from spacy.tokens import Span
 
 
 def cards_untranslated_from_file(
@@ -15,42 +16,29 @@ def cards_untranslated_from_file(
     This is much quicker and avoids 'using up' a DeepL API key if you don't need it"""
     nlp = make_nlp(pipeline)
 
-    prev_span = current_span = None
     cumulative_chars = current_sentence_base = 0
     with open(inputfile, mode="r", encoding="utf-8") as file:
         for line in file:
             docs = nlp.pipe([line.strip()])
 
+            prev_span: Span = None
             for doc in docs:
                 for s in doc.sents:
-                    for next_span in split_sentence(
-                        doc, s, max_span_length=maxfieldlen
-                    ):
-                        if current_span:
-                            yield Card(
-                                inputfile,
-                                current_sentence_base + current_span.start_char,
-                                prev_span.text if prev_span else "",
-                                current_span.text,
-                                next_span.text,
-                                "",
-                            )
-                        prev_span = current_span
-                        current_span = next_span
+                    for span in split_sentence(doc, s, max_span_length=maxfieldlen):
+                        yield Card(
+                            inputfile,
+                            current_sentence_base + span.start_char,
+                            "",
+                            span.text,
+                            "",
+                            "",
+                        )
                         if (
-                            prev_span and current_span.doc is not prev_span.doc
+                            prev_span and span.doc is not prev_span.doc
                         ):  # once current_span moves from one line to the next
                             current_sentence_base = cumulative_chars
+                        prev_span = span
 
                 break  # there was only one doc, hopefully!
             # how many chars to start of current line?
             cumulative_chars = cumulative_chars + len(line.strip())
-
-        yield Card(
-            inputfile,
-            cumulative_chars + current_span.start_char,
-            prev_span.text if prev_span else None,
-            current_span.text,
-            "",
-            "",
-        )
