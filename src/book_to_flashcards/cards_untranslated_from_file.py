537ca5218eb69e64b69496c995e3960a08520f4d
Ashby Winch <github@blueumbrella.net>
2024-08-13 17:56:13 +0100

    Simplified spacy usage (#23)

--- src/book_to_flashcards/cards_untranslated_from_file.py
+++ src/book_to_flashcards/cards_untranslated_from_file.py
@@ -5,7 +5,6 @@ from split_sentences import make_nlp, split_sentence
 
 from collections.abc import Generator
 from typing import Any
-from spacy.tokens import Span
 
 
 def cards_untranslated_from_file(
@@ -17,29 +16,21 @@ def cards_untranslated_from_file(
     This is much quicker and avoids 'using up' a DeepL API key if you don't need it"""
     nlp = make_nlp(pipeline)
 
-    cumulative_chars = current_sentence_base = 0
-    prev_span: Span | None = None
-    with open(inputfile, mode="r", encoding="utf-8") as file:
-        for line in file:
-            docs = nlp.pipe([line.strip()])
+    doc_base = 0
+    
+    file = open(inputfile, mode="r", encoding="utf-8")
+    docs = nlp.pipe(file)
 
-            for doc in docs:
-                for s in doc.sents:
-                    for span in split_sentence(doc, s, max_span_length=maxfieldlen):
-                        if (
-                            prev_span and span.doc is not prev_span.doc
-                        ):  # once current_span moves from one doc to the next
-                            current_sentence_base = cumulative_chars
-                        prev_span = span
-
-                        yield Card(
-                            filename = Path(inputfile).as_posix(),
-                            index_in_file=current_sentence_base + span.start_char,
-                            text = span.text,
-                            translation = ""
-                        )
-                        
-
-                break  # there can only be one doc per line
-            # how many chars to start of current line?
-            cumulative_chars = cumulative_chars + len(line.strip())
+    for doc in docs:
+        for s in doc.sents:
+            for span in split_sentence(doc, s, max_span_length=maxfieldlen):
+                doc_len = span.end_char
+                
+                yield Card(
+                    filename = Path(inputfile).as_posix(),
+                    index_in_file= doc_base + s.start_char + span.start_char,
+                    text = span.text,
+                    translation = ""
+                )
+                
+        doc_base = doc_base + doc_len
