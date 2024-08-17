90deeb7f233ae890548f0bc13f66970800159f1b
Ashby Winch <msn@blueumbrella.net>
2024-08-17 17:32:33 +0100

    Updated complexity calcs to generate jsonl instead of csv

--- test/test_book_complexity.py
+++ test/test_book_complexity.py
@@ -1,5 +1,6 @@
 """Tests for book_complexity module"""
 
+import glob
 import unittest
 
 from book_complexity import get_book_complexity, make_nlp
@@ -7,9 +8,10 @@ from book_complexity.ComplexityCalculators import (
     sentence_grammar_depth,
     sentence_count,
 )
+from book_complexity.book_complexity import get_complexities
 
-
-nlp = make_nlp("ru_core_news_sm")
+nlp_ru = make_nlp("ru_core_news_sm")
+nlp_en = make_nlp("en_core_web_sm")
 
 
 class TestBookComplexityOnShortString(unittest.TestCase):
@@ -17,7 +19,7 @@ class TestBookComplexityOnShortString(unittest.TestCase):
 
     # let's not explode when we see non-Latin-1 text
     short_string = "Дедушка поцеловал Лидиньку"
-    doc = next(nlp.pipe([short_string]))
+    doc = next(nlp_ru.pipe([short_string]))
 
     def test_grammar_depth_simple(self):
         """Are grammar depth correct on a known short string?"""
@@ -38,7 +40,7 @@ class TestBookComplexityOnMultipleLongerStrings(unittest.TestCase):
     ]
 
     def setUp(self):
-        self.complexity = get_book_complexity(self.long_strings, nlp)
+        self.complexity = get_book_complexity(self.long_strings, nlp_ru)
 
     def test_word_count_long(self):
         self.assertEqual(self.complexity["Word Count"], 52)
@@ -55,7 +57,7 @@ class TestBookComplexityOnMultipleLongerStrings(unittest.TestCase):
     def test_known_words(self):
         teststrings = ["Bob likes green peas"]
         vocabulary = {"likes", "peas"}
-        complexity = get_book_complexity(teststrings, nlp, vocabulary=vocabulary)
+        complexity = get_book_complexity(teststrings, nlp_en, vocabulary=vocabulary)
 
         self.assertEqual(complexity["Words Known"], 2)
         self.assertEqual(complexity["Percent Words Known"], 50)
@@ -66,10 +68,20 @@ class TestBookComplexityOnMultipleLongerStrings(unittest.TestCase):
         teststrings = ["Bob likes green peas"]
 
         complexity = get_book_complexity(
-            teststrings, nlp, frequency=frequency, levels=levels
+            teststrings, nlp_en, frequency=frequency, levels=levels
         )
 
         self.assertEqual(complexity["Vocabulary Level"], 1)
+
+
+class test_get_complexities_from_folder(unittest.TestCase):
+    def test_complexities(self):
+        files = glob.glob("test/data/dummy_books/**/*.txt", recursive=True)
+        complexities = list(get_complexities(files, nlp=nlp_en))
+        self.assertEqual(len(list(complexities)), 2)
+        self.assertEqual(complexities[0]["title"], "dummy_book")
+        self.assertEqual(complexities[0]["author"], "dummy_books")
+        self.assertEqual(complexities[0]["lang"], "en")
 
 
 if __name__ == "__main__":
